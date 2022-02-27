2b301d4f946e70ae397fff588c447f216f15ee70
S3cBar0n <secberg@gmail.com>
2022-02-27 05:15:25 -0700

    Parsed data from DB to our monitoring widgets to display accurate information via the DB. (#22)
    * Created requirements.txt
    
    * Added function to be run when the homepage is loaded, this loads the data for our monitor widgets.
    
    Added the fields and dynamic status to change color/emoji whether on/off.

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template
+from db import connect_to_database, select_last_test, select_sites
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=["GET"])
 def index_page():
-    return render_template("index.html")
+    site_status = get_status()
+    return render_template("index.html", site_status=site_status)
 
 
 @app.route('/tech', methods=["GET"])
@@ -21,6 +23,20 @@ def about_page():
 @app.route('/authors', methods=["GET"])
 def authors_page():
     return render_template("authors.html")
+
+
+def get_status():
+    site_status = {}
+    sites = select_sites(connect_to_database())
+    for site in sites:
+        site_report = list(select_last_test(connect_to_database(), site[0]))
+        site_status[site[0]] = {'name': site_report[1],
+                             'response_code': str(site_report[2]),
+                             'score': site_report[3],
+                             'speed_score': site_report[4],
+                             'tti': site_report[5]}
+
+    return site_status
 
 
 if __name__ == '__main__':
