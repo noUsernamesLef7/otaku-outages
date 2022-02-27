8ef66c25ce25168a024a1a9c4aadf431945dcd71
S3cBar0n <secberg@gmail.com>
2022-02-27 03:22:55 -0700

    Removed random "=====" that was causing issues.

--- db.py
+++ db.py
@@ -1,30 +1,36 @@
 import psycopg2
 
+
 def connect_to_database():
     return psycopg2.connect(
-        "sslmode=disable dbname=postgres user=postgres hostaddr=35.221.45.109"
+        "sslmode=disable dbname=postgres user=postgres password=JnKDzxuIAJcyvw6A hostaddr=35.221.45.109"
     )
+
 
 # Takes a db connection, service name, http response code, performance score, speed index score, and time to interactive value, then inserts it in the stats table
 def insert_test(conn, service_name, response_code, score, speed_index_score, tti):
     with conn.cursor() as cur:
-        cur.execute("INSERT INTO stats (name, response_code, score, speed_index_score, tti) VALUES (%(service_name)s, %(response_code)s, %(score)s, %(speed_index_score)s, %(tti)s);", {"service_name":service_name, "response_code":response_code, "score":score, "speed_index_score":speed_index_score, "tti":tti})
+        cur.execute(
+            "INSERT INTO stats (name, response_code, score, speed_index_score, tti) VALUES (%(service_name)s, %(response_code)s, %(score)s, %(speed_index_score)s, %(tti)s);",
+            {"service_name": service_name, "response_code": response_code, "score": score,
+             "speed_index_score": speed_index_score, "tti": tti})
     conn.commit()
     return
+
 
 # Takes a db connection and service name and selects most recent test results of specified service
 def select_last_test(conn, service_name):
     with conn.cursor() as cur:
-        cur.execute("SELECT * FROM stats WHERE name=%(service_name)s AND index = (SELECT MAX(index) FROM stats);", {"service_name":service_name})
+        cur.execute("SELECT * FROM stats WHERE name=%(service_name)s AND index = (SELECT MAX(index) FROM stats);",
+                    {"service_name": service_name})
     return cur.fetchone()
 
+
 # select all checks for a specified service over the specified time period
-=======
-#TODO
 
 # select a list of all sites in the sites table
 def select_sites(conn):
     with conn.cursor() as cur:
         cur.execute("SELECT * FROM sites")
     return cur.fetchall()
- 
+
