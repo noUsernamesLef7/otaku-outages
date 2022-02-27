668478f144c793f97a4c11cf3e24d44055b179e9
Specs64z <59633465+Specs64z@users.noreply.github.com>
2022-02-27 05:29:56 -0600

    Add files via upload
    gottem gg no re

--- gather_info.py
+++ gather_info.py
@@ -1,31 +1,32 @@
 import urllib.request
 import json
 import cloudscraper
-from .. import db
+from . import db
 
 
 def populate_db():
+    print("POP DB")
     # open DB connection
     conn = db.connect_to_database()
     # prep cloudscraper
     scraper = cloudscraper.create_scraper()
 
     # pull list of names/urls from the DB
-    # key = name, value = url
+    # [0] = name, [1] = url
     website_list = db.select_sites(conn)
 
     for site in website_list:
         # check if site is crunchyroll
-        if site.lower() == "crunchyroll":
+        if site[0].lower() == "crunchyroll":
             # use cloudscraper to bypass Crunchyroll lmao gottem ggs no re
             is_up = scraper.get("https://www.crunchyroll.com").status_code
             # insert into DB, adding null values for the ones we can't obtain
-            db.insert_test(conn, site, is_up, None, None, None)
+            db.insert_test(conn, site[0], is_up, None, None, None)
         # use this for every other site
         else:
             # dynamically build URL to request from using Google PageSpeed Insight
             url = "https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=" \
-                  + website_list.get(site) + \
+                  + site[1] + \
                   "&strategy=desktop&key=AIzaSyCQONQHpQ3hIcxf1G-rPtM011kt5qefQRU"
 
             # make request and create object for the json data
@@ -39,7 +40,7 @@ def populate_db():
             time_interactive_score = data["lighthouseResult"]["audits"]["interactive"]["score"]
 
             # insert into DB
-            db.insert_test(conn, site, is_up, overall_score, speed_index_score,
+            db.insert_test(conn, site[0], is_up, overall_score, speed_index_score,
                            time_interactive_score)
     conn.close()
     return
